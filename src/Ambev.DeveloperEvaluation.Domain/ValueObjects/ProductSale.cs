9d3f4c867ceb1c7ee387fecb009043ea697a74d1
Felipe Pinheiro <flpinheiro@gmail.com>
2025-02-16 21:46:30 -0300

    feat: #1 Add entity, Mapping and Entity DBContext
    * Added Entity Classes ( Product, Salee,) and ValueObject (ProductSale)
    * Added Entity Mapping
    * Added property on DefaultContext

---
+++ src/Ambev.DeveloperEvaluation.Domain/ValueObjects/ProductSale.cs
@@ -0,0 +1,76 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Domain.ValueObjects;
+
+public class ProductSale
+{
+    /// <summary>
+    /// Gets the sale id and sale information.
+    /// </summary>
+    public Guid SaleId { get; set; }
+    /// <summary>
+    /// Gets the sale id and sale information.
+    /// </summary>
+    public Sale? Sale { get; set; }
+
+    /// <summary>
+    /// Gets the product id and product information.
+    /// </summary>
+    public Guid ProductId { get; set; }
+    /// <summary>
+    /// Gets the product id and product information.
+    /// </summary>
+    public Product? Product { get; set; }
+
+    /// <summary>
+    /// Gets the product quantity.
+    /// </summary>
+    public int Quantity { get; set; }
+
+    /// <summary>
+    /// Gets the total amount of the sale by Product.
+    /// </summary>
+    public decimal TotalAmout { get; set; }
+
+    /// <summary>
+    /// Gets the discount amount of the sale by Product.
+    /// </summary>
+    public decimal Discount { get; set; }
+
+    /// <summary>
+    /// gets the sale.status of the product
+    /// </summary>
+    public SaleStatus Status { get; set; } = SaleStatus.Active;
+
+    public DateTime Date { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    /// <summary>
+    /// Calculate total ammout for the product
+    /// </summary>
+    public void CalculateTotalAmount()
+    {
+        CalculateDiscount();
+        TotalAmout = Quantity * Product?.Price * (1M - Discount / 100M) ?? 0;
+    }
+
+    /// <summary>
+    /// calculate discount for the product
+    /// </summary>
+    private void CalculateDiscount()
+    {
+        if (Quantity >= 10 && Quantity <= 20)
+        {
+            Discount = 20;
+            return;
+        }
+        if (Quantity >= 4 && Quantity < 10)
+        {
+            Discount = 10;
+            return;
+        }
+        Discount = 0;
+    }
+}
