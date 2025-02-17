f631dbd09f1737b8fac3d0636f24ee201b019ca7
Felipe Pinheiro <flpinheiro@gmail.com>
2025-02-16 23:24:07 -0300

    feate: #3 add repository
    * add product repository
    * add sale repository
    * add soe unit test

---
+++ tests/Ambev.DeveloperEvaluation.Unit/Domain/ValueObjects/TestData/ProductSaleTestData.cs
@@ -0,0 +1,27 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+using Ambev.DeveloperEvaluation.Unit.Domain.Entities.TestData;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.ValueObjects.TestData;
+
+public static class ProductSaleTestData
+{
+    private static readonly Faker<ProductSale> ProductSaleFaker = new Faker<ProductSale>()
+        .RuleFor(p => p.Quantity, f => f.Random.Number(1, 20))
+        .RuleFor(p => p.Product, ProductTestData.GenerateValidProduct())
+        .RuleFor(p => p.ProductId, (f, c) => c.Product?.Id ?? f.Random.Guid());
+
+
+    private static Faker<ProductSale> WithSale(this Faker<ProductSale> faker, Sale sale) => faker
+        .RuleFor(p => p.Sale, sale)
+        .RuleFor(p => p.SaleId, sale.Id);
+
+    public static ProductSale GenerateValidProductSale() => ProductSaleFaker.Generate();
+
+    public static ProductSale GenerateValidProductSale(Sale sale) => ProductSaleFaker.WithSale(sale).Generate();
+
+    public static IEnumerable<ProductSale> GenerateValidProductSales(int count) => ProductSaleFaker.Generate(count);
+
+    public static IEnumerable<ProductSale> GenerateValidProductSales(int count, Sale sale) => ProductSaleFaker.WithSale(sale).Generate(count);
+}
